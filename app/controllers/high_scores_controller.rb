a10ef3074bc98095390be1341027e94d335ec07b
mfjinreno <mfjnvbell@gmail.com>
2016-05-05 23:30:39 -0400

    deleted commented out ranking sort system on high_scores_controller

--- app/controllers/high_scores_controller.rb
+++ app/controllers/high_scores_controller.rb
@@ -22,23 +22,5 @@ class HighScoresController < ApplicationController
         stat_hash.each do |key, value|
           puts('key='+key+', value='+value)
         end
-    end
-    # returns a hash of arrays sorted with the top score in the first place
-=begin
-     def _sorted_bugs
-        @sorted_bugs=Hash.new
-        sorted_bug_stats = _sorted_bug_stats
-        sorted_bug_stats.each do |key,value|
-            bug = @bugs.where(id: key).first
-            bug_stat = @bug_stats.where(bug_id: key).first
-            if !bug.blank?
-                @sorted_bugs[bug.bug_name] = [bug.bug_name,bug.email,bug_stat.wins,
-                                              bug_stat.losses,bug_stat.current_streak,bug_stat.longest_streak]
-            end
-        end
-        @sorted_bugs
-    end
-=end
-
-        
+    end 
 end
